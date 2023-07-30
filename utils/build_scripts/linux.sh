53a32412388b7419e04c272fe7f9a7f9b884703f
MrKris7100 <czacha55@vp.pl>
2023-07-30 16:18:47 +0200

    Added arguments to linux build script

--- utils/build_scripts/linux.sh
+++ utils/build_scripts/linux.sh
@@ -1,27 +1,48 @@
 #Morelo build script for linux
 #Copyrights Morelo Network 2023
 
+nodeps=false
+nocmake=false
+noboost=false
+
+for arg in "$@"
+do
+    if [ $arg = "--no-dependences" ]; then
+		nodeps=true
+	elif [ $arg = "--no-cmake" ]; then
+		nocmake=true
+	elif [ $arg = "--no-boost" ]; then
+		noboost=true
+	fi
+done
+
 #Install dependences
-sudo apt update && sudo apt install --yes git build-essential curl pkg-config libssl-dev libsodium-dev libunwind-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev doxygen graphviz libudev-dev libusb-1.0-0-dev libhidapi-dev xsltproc gperf autoconf automake libtool-bin
+if [ $nodeps = false ] ; then
+	sudo apt update && sudo apt install --yes git build-essential curl pkg-config libssl-dev libsodium-dev libunwind-dev liblzma-dev libreadline-dev libldns-dev libexpat1-dev doxygen graphviz libudev-dev libusb-1.0-0-dev libhidapi-dev xsltproc gperf autoconf automake libtool-bin
+fi
 
 #Build and install cmake 3.17.3 from source
-wget https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz
-tar -xvf cmake-3.17.3.tar.gz
-cd cmake-3.17.3
-./bootstrap
-make -j4
-sudo make install
-cd ..
+if [ $nocmake = false ] ; then
+	wget https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz
+	tar -xvf cmake-3.17.3.tar.gz
+	cd cmake-3.17.3
+	./bootstrap
+	make -j4
+	sudo make install
+	cd ..
+fi
 
 #Build and install boost 1.73.0 from source
-wget https://boostorg.jfrog.io/artifactory/main/release/1.73.0/source/boost_1_73_0.tar.gz
-tar -xvf boost_1_73_0.tar.gz
-cd boost_1_73_0
-./bootstrap.sh
-./b2 -j4
-sudo ./b2 install
-sudo ./b2 headers
-cd ..
+if [ $noboost = false ] ; then
+	wget https://boostorg.jfrog.io/artifactory/main/release/1.73.0/source/boost_1_73_0.tar.gz
+	tar -xvf boost_1_73_0.tar.gz
+	cd boost_1_73_0
+	./bootstrap.sh
+	./b2 -j4
+	sudo ./b2 install
+	sudo ./b2 headers
+	cd ..
+fi
 
 #Build morelo from source
 git clone https://github.com/MoreloNetwork/morelo --recursive
