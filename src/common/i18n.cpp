086799bc23bf1b71dbb3444c205da2dc10b79a23
MrKris7100 <czacha55@vp.pl>
2023-07-31 03:35:03 +0200

    Added missing include

--- src/common/i18n.cpp
+++ src/common/i18n.cpp
@@ -35,6 +35,7 @@
 #include "file_io_utils.h"
 #include "common/i18n.h"
 #include "translation_files.h"
+#include <algorithm>
 
 #undef ARQMA_DEFAULT_LOG_CATEGORY
 #define ARQMA_DEFAULT_LOG_CATEGORY "i18n"
